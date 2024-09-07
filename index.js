880d752c6c08e4ca9687c85bbbc87d2e35c76d3e
Nizo-Developer <nizo5671@gmail.com>
2024-09-07 14:18:49 +0700

    V1.2
    security

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 console.log(navigator.userAgent)
 
 const browser = navigator.userAgent
+const location = window.location.search.slice(1).split('=')
 
 document.addEventListener('DOMContentLoaded', () => {
   const x = document.querySelector('#a')
-  if (browser.includes('Version/4.0') && window.location.search) {
+  if (browser.includes('Version/4.0') && window.location.search && location[0] == 'MSYD') {
 
-    const location = window.location.search.slice(1).split('=')[1]
     x.innerHTML = 'Selamat Mengerjakan'
 
-    window.location.href = `https://docs.google.com/forms/d/e/${location}/viewform?usp=sf_link`
+    window.location.href = `https://docs.google.com/forms/d/e/${location[1]}/viewform?usp=sf_link`
   } else if (!window.location.search) {
 
     x.innerHTML = 'Ups Bertemu Jurang\nURL Tidak Valid'
