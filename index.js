d616f4ab4c5e882aa6f0d663c57d835b13297898
Nizo-Developer <nizo5671@gmail.com>
2024-09-07 14:29:03 +0700

    V1.2.2
    bug

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-console.log(navigator.userAgent)
-
-const browser = navigator.userAgent
-const location = window.location.search.slice(1).split('=')
+const browser = navigator.userAgent;
+const loc = window.location.search.slice(1).split('=');
 
 document.addEventListener('DOMContentLoaded', () => {
   const x = document.querySelector('#a')
-  if (browser.includes('Version/4.0') && window.location.search && location[0] == 'MSYD') {
+  if (browser.includes('Version/4.0') && window.location.search && loc[0] == 'MSYD') {
 
     x.innerHTML = 'Selamat Mengerjakan'
 
-    window.location.href = `https://docs.google.com/forms/d/e/${location[1]}/viewform?usp=sf_link`
+    window.location.href = `https://docs.google.com/forms/d/e/${loc[1]}/viewform?usp=sf_link`
   } else if (!window.location.search) {
 
     x.innerHTML = 'Ups Bertemu Jurang\nURL Tidak Valid'
