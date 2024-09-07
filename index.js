b1e14595d0839d6c114c3f00c821056042c51aad
Nizo-Developer <nizo5671@gmail.com>
2024-09-07 14:37:20 +0700

    V1.2.3
    fixing

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const x = document.querySelector('#a')
   if (browser.includes('Version/4.0') && window.location.search && loc[0] == 'MSYD') {
 
-    x.innerHTML = 'Selamat Mengerjakan'
+    x.innerHTML = 'Selamat Mengerjakan';
 
-    window.location.href = `https://docs.google.com/forms/d/e/${loc[1]}/viewform?usp=sf_link`
-  } else if (!window.location.search) {
+    window.location.href = `https://docs.google.com/forms/d/e/${loc[1]}/viewform?usp=sf_link`;
+  } else if (!window.location.search || loc[0] !== 'MSYD') {
 
-    x.innerHTML = 'Ups Bertemu Jurang\nURL Tidak Valid'
+    x.innerHTML = 'Ups Bertemu Jurang\n URL-mu Tidak Valid';
 
   } else {
-    x.innerHTML = 'Jangan Curang, Gunakan Exambro!'
+    x.innerHTML = 'Silakan Akses Soal Melalui Exambro!';
   }
 
 })
