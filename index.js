b21c53117bd283d3318fb189ef5842b6477d160e
Nizo-Developer <nizo5671@gmail.com>
2024-09-07 14:16:01 +0700

    V1.1

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 console.log(navigator.userAgent)
 
-const browser = Object.values(navigator)
-const x = navigator
-
-console.log(x)
-alert(navigator.userAgent)
-// navigator.clipboard.writeText(browser)
+const browser = navigator.userAgent
 
 document.addEventListener('DOMContentLoaded', () => {
   const x = document.querySelector('#a')
-  // if (browser.includes('tp_bridge')) {
+  if (browser.includes('Version/4.0') && window.location.search) {
 
-  //   x.innerHTML = browser;
-  // }
-  x.innerHTML += navigator.deviceMemory + ' ';
-  x.innerHTML += navigator.platform + ' ';
-  x.innerHTML += 'nope'
+    const location = window.location.search.slice(1).split('=')[1]
+    x.innerHTML = 'Selamat Mengerjakan'
+
+    window.location.href = `https://docs.google.com/forms/d/e/${location}/viewform?usp=sf_link`
+  } else if (!window.location.search) {
+
+    x.innerHTML = 'Ups Bertemu Jurang\nURL Tidak Valid'
+
+  } else {
+    x.innerHTML = 'Jangan Curang, Gunakan Exambro!'
+  }
 
 })
